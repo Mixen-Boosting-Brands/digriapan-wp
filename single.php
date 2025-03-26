f5f2ce07d48201d6cd141b9f594b7fca94f432e8
Luis Carlos Pando <hey@luiscarlospando.com>
2025-03-26 11:08:21 -0600

    Armar single

--- single.php
+++ single.php
@@ -1,71 +1,85 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-	<!-- section -->
-	<section>
+    <section id="jumbotron-interna" class="bg-primary text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <h1 class="mb-4" data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="0"><?php the_title(); ?></h1>
+                </div>
+            </div>
+        </div>
+    </section>
 
-	<?php if ( have_posts() ) : while (have_posts() ) : the_post(); ?>
+	<?php if (have_posts()):
+     while (have_posts()):
+         the_post(); ?>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <section class="py-60">
+            <div class="container">
+                <div class="row">
+                    <div class="col col-lg-9 offset-lg-2 text-center">
+                        <figure
+                            data-aos="fade-up"
+                            data-aos-duration="1000"
+                            data-aos-delay="100"
+                        >
+                            <?php the_post_thumbnail("full", [
+                                "class" => "img-fluid",
+                            ]); ?>
+                        </figure>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <?php esc_html_e(
+                                    "Por",
+                                    "html5blank"
+                                ); ?> <?php the_author_posts_link(); ?>
+                            </li>
+                            <li class="list-inline-item">
+                                ·
+                            </li>
+                            <li class="list-inline-item">
+                                <time datetime="<?php the_time(
+                                    "Y-m-d"
+                                ); ?> <?php the_time("H:i"); ?>">
+                                   	<?php the_date(); ?> <?php the_time(); ?>
+                				</time>
+                            </li>
+                        </ul>
+                        <?php the_content();
+         // Dynamic Content.
+         ?>
+                        <?php edit_post_link();
+         // Always handy to have Edit Post Links available.
+         ?>
+                    </div>
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail() ) : // Check if Thumbnail exists. ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post. ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+                </div>
+            </div>
+        </section>
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+	<?php
+     endwhile; ?>
+	<?php
+ else:
+      ?>
 
-			<!-- post details -->
-			<span class="date">
-				<time datetime="<?php the_time( 'Y-m-d' ); ?> <?php the_time( 'H:i' ); ?>">
-					<?php the_date(); ?> <?php the_time(); ?>
-				</time>
-			</span>
-			<span class="author"><?php esc_html_e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if ( comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' ) ); ?></span>
-			<!-- /post details -->
+        <section class="py-60">
+            <div class="container">
+                <div class="row">
+                    <div class="col col-lg-9 offset-lg-2 text-center">
+                        <?php esc_html_e(
+                            "Lo sentimos, no hay nada que mostrar aquí.",
+                            "html5blank"
+                        ); ?>
+                    </div>
 
-			<?php the_content(); // Dynamic Content. ?>
+                </div>
+            </div>
+        </section>
 
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>' ); // Separated by commas with a line break at the end. ?>
-
-			<p><?php esc_html_e( 'Categorised in: ', 'html5blank' ); the_category( ', ' ); // Separated by commas. ?></p>
-
-			<p><?php esc_html_e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
-
-			<?php edit_post_link(); // Always handy to have Edit Post Links available. ?>
-
-			<?php comments_template(); ?>
-
-		</article>
-		<!-- /article -->
-
-	<?php endwhile; ?>
-
-	<?php else : ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php esc_html_e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+	<?php
+ endif; ?>
 
 <?php get_footer(); ?>
