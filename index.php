bdef94e5d7f8c074152ab7604717e875402a9764
Luis Carlos Pando <hey@luiscarlospando.com>
2025-04-01 12:18:24 -0600

    Avance hasta: 1 Apr 2025 at 12:18 p.m.

--- index.php
+++ index.php
@@ -518,7 +518,7 @@
             </div>
             <div class="col-12 col-lg-6 my-auto">
                 <a
-                    href="#"
+                    href="<?php echo esc_url(get_permalink(7)); ?>"
                     class="btn btn-secondary btn-lg rounded-pill"
                     data-aos="fade-up"
                     data-aos-duration="1000"
