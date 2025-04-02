2add9ccc1b634b8d6993cd32f2326cc01662d727
Luis Carlos Pando <hey@luiscarlospando.com>
2025-04-02 11:11:21 -0600

    Avance hasta: 2 Apr 2025 at 11:09 a.m.

--- functions.php
+++ functions.php
@@ -659,8 +659,8 @@ add_action("wp_enqueue_scripts", "cargar_estilos_js_woocommerce");
 // WooCommerce - Cart updating
 add_filter("woocommerce_add_to_cart_fragments", function ($fragments) {
     $fragments[".cart-count"] =
-        '<span class="cart-count">' .
+        '(<span class="cart-count">' .
         WC()->cart->get_cart_contents_count() .
-        "</span>";
+        "</span>)";
     return $fragments;
 });
