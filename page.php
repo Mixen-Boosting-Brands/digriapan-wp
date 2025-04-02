7175e625462943488cb3422548f05c0f3313d8a8
Luis Carlos Pando <hey@luiscarlospando.com>
2025-04-02 11:45:44 -0600

    Avance hasta: 2 Apr 2025 at 11:45 a.m.

--- page.php
+++ page.php
@@ -1,9 +1,18 @@
 <?php get_header(); ?>
 
-    <section id="jumbotron-interna" class="<?php if (is_woocommerce()) {
+    <section id="jumbotron-interna" class="<?php if (
+        function_exists("is_woocommerce") &&
+        (is_woocommerce() || is_cart() || is_checkout() || is_account_page())
+    ) {
         echo "bg-primary";
-    } ?> text-white" <?php if (is_page() && !is_woocommerce()) {
-     // Verifica si es página pero NO es de WooCommerce
+    } ?> text-white" <?php if (
+     (is_page() && !function_exists("is_woocommerce")) ||
+     (function_exists("is_woocommerce") &&
+         !is_woocommerce() &&
+         !is_cart() &&
+         !is_checkout() &&
+         !is_account_page())
+ ) {
      echo 'style="background: url(' .
          get_the_post_thumbnail_url(get_the_ID(), "full") .
          ') no-repeat; background-size: cover;"';
@@ -15,12 +24,16 @@
                     <h1 class="mb-4" data-aos="fade-up"
                     data-aos-duration="1000"
                     data-aos-delay="0">
-                        <?php if (is_cart()) {
-                            echo "Carrito de Compras";
-                        } elseif (is_checkout()) {
-                            echo "Finalizar Compra";
-                        } elseif (is_account_page()) {
-                            echo "Mi Cuenta";
+                        <?php if (function_exists("is_woocommerce")) {
+                            if (is_cart()) {
+                                echo "Carrito de Compras";
+                            } elseif (is_checkout()) {
+                                echo "Finalizar Compra";
+                            } elseif (is_account_page()) {
+                                echo "Mi Cuenta";
+                            } else {
+                                the_title();
+                            }
                         } else {
                             the_title();
                         } ?>
@@ -36,12 +49,51 @@
         <section class="py-60">
             <div class="container">
                 <div class="row">
-                    <div class="col <?php echo is_woocommerce()
-                        ? "col-12"
-                        : "col-lg-9 offset-lg-2"; ?>">
+                    <div class="col <?php if (
+                        function_exists("is_woocommerce") &&
+                        (is_woocommerce() ||
+                            is_cart() ||
+                            is_checkout() ||
+                            is_account_page())
+                    ) {
+                        echo "col-12";
+                    } else {
+                        echo "col-lg-9 offset-lg-2";
+                    } ?>">
+                        <?php if (
+                            !function_exists("is_woocommerce") ||
+                            (function_exists("is_woocommerce") &&
+                                !is_woocommerce() &&
+                                !is_cart() &&
+                                !is_checkout() &&
+                                !is_account_page())
+                        ): ?>
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                    <?php esc_html_e("Por", "html5blank"); ?>
+                                    <?php the_author_posts_link(); ?>
+                                </li>
+                                <li class="list-inline-item">·</li>
+                                <li class="list-inline-item">
+                                    <time datetime="<?php the_time(
+                                        "Y-m-d"
+                                    ); ?> <?php the_time("H:i"); ?>">
+                                        <?php the_date(); ?> <?php the_time(); ?>
+                                    </time>
+                                </li>
+                            </ul>
+                        <?php endif; ?>
+
                         <?php the_content(); ?>
 
-                        <?php if (!is_woocommerce()): ?>
+                        <?php if (
+                            !function_exists("is_woocommerce") ||
+                            (function_exists("is_woocommerce") &&
+                                !is_woocommerce() &&
+                                !is_cart() &&
+                                !is_checkout() &&
+                                !is_account_page())
+                        ): ?>
                             <?php edit_post_link(); ?>
                         <?php endif; ?>
                     </div>
