fe2c66a72a31581849fbf3c80c76e1f6631e8446
Luis Carlos Pando <hey@luiscarlospando.com>
2025-04-02 11:37:48 -0600

    Avance hasta: 2 Apr 2025 at 11:37 a.m.

--- single.php
+++ single.php
@@ -1,70 +1,73 @@
 <?php get_header(); ?>
 
-    <section id="jumbotron-interna" class="<?php if (
-        is_woocommerce()
-    ): ?>bg-primary<?php endif; ?> text-white" <?php if (
-     is_single()
- ): ?>style="background: url('<?php echo get_the_post_thumbnail_url(
-    get_the_ID(),
-    "full"
-); ?>') no-repeat; background-size: cover;"<?php endif; ?>>
+    <section id="jumbotron-interna" class="<?php if (is_woocommerce()) {
+        echo "bg-primary";
+    } ?> text-white" <?php if (is_single() && !is_product()) {
+     // Verifica si es single pero NO es un producto
+     echo 'style="background: url(' .
+         get_the_post_thumbnail_url(get_the_ID(), "full") .
+         ') no-repeat; background-size: cover;"';
+ } ?>>
         <div class="overlay"></div>
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <h1 class="mb-4" data-aos="fade-up"
                     data-aos-duration="1000"
-                    data-aos-delay="0"><?php the_title(); ?></h1>
+                    data-aos-delay="0">
+                        <?php if (is_cart()) {
+                            echo "Carrito de Compras";
+                        } elseif (is_checkout()) {
+                            echo "Finalizar Compra";
+                        } else {
+                            the_title();
+                        } ?>
+                    </h1>
                 </div>
             </div>
         </div>
     </section>
 
-	<?php if (have_posts()):
-     while (have_posts()):
-         the_post(); ?>
-
+    <?php if (have_posts()):
+        while (have_posts()):
+            the_post(); ?>
         <section class="py-60">
             <div class="container">
                 <div class="row">
-                    <div class="col col-lg-9 offset-lg-2">
+                    <div class="col <?php echo is_product()
+                        ? "col-12"
+                        : "col-lg-9 offset-lg-2"; ?>">
                         <?php if (!is_woocommerce()): ?>
                             <ul class="list-inline">
                                 <li class="list-inline-item">
-                                    <?php esc_html_e(
-                                        "Por",
-                                        "html5blank"
-                                    ); ?> <?php the_author_posts_link(); ?>
+                                    <?php esc_html_e("Por", "html5blank"); ?>
+                                    <?php the_author_posts_link(); ?>
                                 </li>
-                                <li class="list-inline-item">
-                                    ·
-                                </li>
+                                <li class="list-inline-item">·</li>
                                 <li class="list-inline-item">
                                     <time datetime="<?php the_time(
                                         "Y-m-d"
                                     ); ?> <?php the_time("H:i"); ?>">
-                                       	<?php the_date(); ?> <?php the_time(); ?>
-                    				</time>
+                                        <?php the_date(); ?> <?php the_time(); ?>
+                                    </time>
                                 </li>
                             </ul>
                         <?php endif; ?>
-                        <?php the_content();
-         // Dynamic Content.
-         ?>
-                        <?php edit_post_link();
-         // Always handy to have Edit Post Links available.
-         ?>
+
+                        <?php the_content(); ?>
+
+                        <?php if (!is_woocommerce()): ?>
+                            <?php edit_post_link(); ?>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
         </section>
-
-	<?php
-     endwhile; ?>
-	<?php
- else:
-      ?>
-
+    <?php
+        endwhile; ?>
+    <?php
+    else:
+         ?>
         <section class="py-60">
             <div class="container">
                 <div class="row">
@@ -74,12 +77,10 @@
                             "html5blank"
                         ); ?>
                     </div>
-
                 </div>
             </div>
         </section>
-
-	<?php
- endif; ?>
+    <?php
+    endif; ?>
 
 <?php get_footer(); ?>
